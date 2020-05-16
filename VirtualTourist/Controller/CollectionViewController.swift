4afea2a91e4c810ad56e6755f5061a5b5f9de2eb
Huong-Tran <huongttl92@gmail.com>
2020-05-17 01:54:42 +0900

    Project init with mapView gesture

---
+++ VirtualTourist/Controller/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by Huong Tran on 5/16/20.
+//  Copyright © 2020 RiRiStudio. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VirtualTourist/Controller/MapViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Huong Tran on 5/16/20.
+//  Copyright © 2020 RiRiStudio. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleTap(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 0.6
+        gestureRecognizer.delegate = self
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    @objc func handleTap(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let location = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+
+            // Add annotation:
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+        }
+//        if gestureRecognizer.state == .ended {
+//
+//        }
+    }
+
+    @IBAction func move(_ sender: Any) {
+    let nextVC = storyboard?.instantiateViewController(identifier: "CollectionViewController") as! CollectionViewController
+    navigationController?.pushViewController(nextVC, animated: true)
+
+    }
+    
+    @IBAction func nextView(_ sender: Any) {
+        let nextVC = storyboard?.instantiateViewController(identifier: "CollectionViewController") as! CollectionViewController
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let pinId = "PinAnnotationIdentifier"
+        let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: pinId)
+        pinView.animatesDrop = true
+        pinView.canShowCallout = true
+        pinView.annotation = annotation
+        pinView.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        print("latitude: \(annotation.coordinate.latitude), longitude: \(annotation.coordinate.longitude)")
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("pin selected")
+    }
+}
